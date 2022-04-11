a66917f207e8c0ecff9b9765025213923c3af0ce
HyeogDo <peterhd@daum.net>
2022-04-11 10:54:31 +0900

    [update] 2022/04/11 잘못 수정된 내용 fix

--- src/main/java/com/betterhip/dao/order/CakeInfoDao.java
+++ src/main/java/com/betterhip/dao/order/CakeInfoDao.java
@@ -45,62 +45,7 @@ public class CakeInfoDao {
 		ResultSet resultSet = null;
 		
 		
-		try {
-			connection = dataSource.getConnection();
-			String query = "select cake_id, cake_name, cake_saleprice, cake_status, cake_img from cake order by ? limit ?, ?";
-			preparedStatement = connection.prepareStatement(query);
-			
-			
-			preparedStatement.setString(1, order);
-			preparedStatement.setInt(2, start);
-			preparedStatement.setInt(3, len);
-			
-			preparedStatement.executeUpdate();
-			
-			resultSet = preparedStatement.executeQuery();
-			while(resultSet.next()) {
-				int cake_id = resultSet.getInt("cake_id");
-				String cake_name = resultSet.getString("cake_name");
-				int cake_saleprice = resultSet.getInt("cake_saleprice");
-				String cake_status = resultSet.getString("cake_status");
-				Blob cake_img = resultSet.getBlob("cake_img");
-				
-				// blob 파일 내용 -> bytes
-				InputStream inputStream = cake_img.getBinaryStream();
-				ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-				byte[] buffer = new byte[4096];
-				int bytesRead = -1;
-				
-				while((bytesRead = inputStream.read(buffer)) != -1) {
-					outputStream.write(buffer, 0, bytesRead);
-				}
-				
-				byte[] imageBytes = outputStream.toByteArray();
-				
-				//바이트 -> 문자열
-				String base64Image = Base64.getEncoder().encodeToString(imageBytes);
-				
-				inputStream.close();
-				outputStream.close();
-				
-				CakeListDto cakeListDto = new CakeListDto(cake_id, cake_name, cake_saleprice, cake_status, base64Image);
-				dtos.add(cakeListDto);
-			}
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}finally {
-			try {
-				if(resultSet != null) resultSet.close();
-				if(preparedStatement != null) preparedStatement.close();
-				if(connection != null) connection.close();
-			} catch (Exception e) {
-				// TODO: handle exception
-				e.printStackTrace();
-			}
-			
-		}
+		
 		
 		
 		return dtos;
